87b5cc8aeece48359430f3038b2170b14d5de257
Eliseu Borges <contato@eliseuborges.com>
2018-11-14 15:27:20 -0200

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ echo $_SERVER['SERVER_ADDR'];
 echo "<h1> Secrets </h1><br>";
 echo "<pre>";
 print_r($_ENV); echo "<br>";
-echo getEnv('usuario-banco').":".getEnv('password-banco');
+echo getEnv('username').":".getEnv('password');
 ?>
