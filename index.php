7fb2202b57f0e48a7c8f24e7424eb86164ecd7fd
Eliseu Borges <contato@eliseuborges.com>
2018-11-14 15:20:56 -0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ echo "<h1>Openshift Workshop v3.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 
 echo "<h1> Secrets </h1><br>";
-echo $_ENV."<br>";
+print_r($_ENV); echo "<br>";
 echo getEnv('usuario-banco').":".getEnv('password-banco');
 ?>
